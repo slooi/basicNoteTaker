47fc5d4bec3298dd4af5ca9a104691357af6b7d3
s <s.looi2@outlook.com>
2024-12-17 18:53:12 +0900

    :sparkles: add style toggling and editor toolbar

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
-import { BaseEditor, Descendant, Editor, Transforms, createEditor, Element } from 'slate'
+import { BaseEditor, Descendant, Editor, Transforms, createEditor, Element as SlateElement } from 'slate'
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
 import { useCallback, useState } from 'react'
 
-// ??????????????????????????????????????????????????
+
+
+
 type CustomText = { text: string, bold?: boolean }
 type CustomElement = { type: 'paragraph'; children: CustomText[] }
 type CodeElement = { type: 'code'; children: CustomText[] }
@@ -16,7 +18,62 @@ declare module 'slate' {
     Element: CustomElement | CodeElement
   }
 }
-// ??????????????????????????????????????????????????
+
+
+
+
+
+
+
+
+// Define our own custom set of helpers.
+const CustomEditor = {
+  // Helpers
+  isBoldMarkActive(editor: ReactEditor) {
+    const marks = Editor.marks(editor)
+    return marks ? marks.bold === true : false
+  },
+  isCodeBlockActive(editor: ReactEditor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => SlateElement.isElement(n) && n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  // Togglers
+  toggleBoldMark(editor: ReactEditor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    if (isActive) {
+      Editor.removeMark(editor, 'bold')
+    } else {
+      Editor.addMark(editor, 'bold', true)
+    }
+  },
+  toggleCodeBlock(editor: ReactEditor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? undefined : 'code' }, //!@#!@#!@# changed null to undefined. idk if this will should work or will cause future issues
+      { match: n => SlateElement.isElement(n) && Editor.isBlock(editor, n) }
+    )
+  },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const initialValue: Descendant[] = [{
@@ -24,20 +81,6 @@ const initialValue: Descendant[] = [{
   children: [{ text: 'A line of text in a paragraph.' }],
 }]
 
-
-
-// Define a React component renderer for our code blocks.
-const CodeElement = (props: any) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = (props: any) => {
-  return <p {...props.attributes}>{props.children}</p>
-}
 
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()))
@@ -56,7 +99,29 @@ const App = () => {
     return <Leaf {...props} />
   }, [])
 
-  return <Slate editor={editor} initialValue={initialValue} >
+  return <Slate editor={editor} initialValue={initialValue}>
+
+    <div>
+      <button
+        onMouseDown={event => {
+          event.preventDefault()
+          CustomEditor.toggleBoldMark(editor)
+        }}
+      >
+        Bold
+      </button>
+      <button
+        onMouseDown={event => {
+          event.preventDefault()
+          CustomEditor.toggleCodeBlock(editor)
+        }}
+      >
+        Code Block
+      </button>
+    </div>
+
+
+
     <Editable
       renderLeaf={renderLeaf}
       renderElement={renderElement}
@@ -69,28 +134,21 @@ const App = () => {
         e.preventDefault()
         switch (e.key) {
           case "`":
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: n => Element.isElement(n) && n.type === 'code',
-            })
-
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? 'paragraph' : 'code' },
-              { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
-            )
+            CustomEditor.toggleCodeBlock(editor)
             break
           case "b":
-            Editor.addMark(editor, 'bold', true)
+            CustomEditor.toggleBoldMark(editor)
             break
-          default:
-            throw new Error("UNEXPECTED ERROR HAS OCCURRED. NOT IMPLEMENTED")
         }
       }}
     />
   </Slate>
 }
+
+
+
+
+
 
 // Define a React component to render leaves with bold text.
 const Leaf = (props: any) => {
@@ -102,6 +160,19 @@ const Leaf = (props: any) => {
       {props.children}
     </span>
   )
+}
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>
 }
 
 export default App
